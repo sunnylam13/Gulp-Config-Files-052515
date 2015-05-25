c1ab6cd423944490cfacf0df52917d1d4cebc841
Sunny Lam <sunny.lam@gmail.com>
2015-05-25 12:06:48 -0400

    updated the comments on the gulp file.

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ There are several advantages...
 * Extremely powerful customization -- you can design a setup to suit your specific style or project.  (It's like having an app for almost everything.)
 * Large and long term community support. (New plugins are designed often and are easily available.)
 * Avoid RAM hogging, frozen precompiler issues that require you to force quit.  (With Terminal you simply cut off the problem with a single keystroke.)
+
+# How to Use These Files
+
+Simply use it as a reference or guide about how to setup a basic Gulp system that will process SASS/SCSS, Jade/HTML and Javascript.
+
+This setup uses Browser Sync and is written with the "server" task so that ANY change to SCSS/SASS files, Jade/HTML files or Javascript files causes your previewed web page to reload.
+
+Browser Sync displays your previewed web site by default at `localhost: 3000`.
+
+Note that `*.scss`, `*.jade`, `*.html` uses a wildcard `*` to say, "Look for *any* file with that extension."
+
+**NOTE**:  If you don't use the Node templating language Jade, simply comment out or delete any mentions of Jade in the Gulp file.
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 
 // NOTE:  gulpfile.js must be in the main directory
 
+// if you don't know what these gulp plugins do you can Google them
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
@@ -42,18 +43,21 @@ gulp.task('jade', function() {
 
 gulp.task('sass', function () {
 	return gulp.src('css/*.scss')
+				// Anne's recommendation is to use plumber() to automatically keep Gulp running despite any errors... you're not automatically booted out
 				.pipe(plumber())
 				.pipe(sass({
 					'sourcemap=none':true,
 					'errLogToConsole':true
 				}))
 				.pipe(concat('style.css'))
+				// automatically add vendor prefixes to your CSS so you never have to write them
 				.pipe(autoprefixer({
 		            browsers: ['last 2 versions'],
 		            cascade: false
 		        }))
 		        // .pipe(minifyCss({compatibility: 'ie8'}))
 				.pipe(gulp.dest('css/'))
+				// you need this for BrowserSync so it can auto update your CSS in the browser
 				.pipe(browserSync.stream());
 
 });
@@ -73,22 +77,38 @@ gulp.task('sass', function () {
 
 gulp.task('server', ['sass','jade'], function() {
 
+	// this starts up your Browser Sync server so that you can preview your web page... that's all it does... start the ignition
     browserSync.init({
         server: "./",
     });
 
+    // this assumes you have a ./css/ folder with style.scss and style.css
     gulp.watch("css/*.scss", ['sass']);
+    
     // to get SASS partials to trigger changes
     // the SCSS partials need to be in their own folder because css/*.scss causes all of them to trigger in the same directory, in the order they currently are which messes up everything
+    // this assumes that you have a ./css/partials folder with all your SCSS partial files
     gulp.watch("css/partials/*.scss", ['sass']);
+    
+    // this assumes you have .jade files in your main directory (./)
     gulp.watch('./*.jade',['jade']);
+    
     // to get jade partials to trigger changes
+    // this assumes you have jade partials (similar to SCSS partials) in a ./includes/ folder
     gulp.watch('includes/*.jade',['jade']);
+    
     // whenever the .js files change reload
+    // this assumes you have a ./js/ folder
+    // it is setup to watch changes to 'those' files in 'that' folder
     gulp.watch("js/*.js").on('change', reload);
+    
     // whenever the .css file changes reload
+    // it is setup to watch changes to 'those' files in 'that' folder
     gulp.watch("css/*.css").on('change', reload);
+    
     // whenever the .html file changes reload
+    // this only watches changes to .html files in your main directory (./)
+    // it is setup to watch changes to 'those' files in 'that' folder
     gulp.watch("*.html").on('change', reload);
 });
 
@@ -104,6 +124,7 @@ gulp.task('server', ['sass','jade'], function() {
 
 gulp.task('default', ['server'], function () {	
 	// place everything in here in 'server'
+	// if you weren't using Browser Sync's server
 });
 
 ////////////////////////////////////////////
